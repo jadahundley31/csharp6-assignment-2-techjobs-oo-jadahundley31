b30e95aadab1872cd30154c83e7c5bb37ab285ca
jadahundley31 <jadahundley@gmail.com>
2023-10-02 20:46:22 -0500

    passed test task 3

--- TechJobsOOAutoGraded6/Job.cs
+++ TechJobsOOAutoGraded6/Job.cs
@@ -3,25 +3,48 @@ namespace TechJobsOOAutoGraded6
 {
 	public class Job
 	{
-        /*
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Name { get; set; }
+        public Employer EmployerName { get; set; }
+        public Location EmployerLocation { get; set; }
+        public PositionType JobType { get; set; }
+        public CoreCompetency JobCoreCompetency { get; set; }
 
-            public int Id { get; }
-            private static int nextId = 1;
-            public string Name { get; set; }
-            public Employer EmployerName { get; set; }
-            public Location EmployerLocation { get; set; }
-            public PositionType JobType { get; set; }
-            public CoreCompetency JobCoreCompetency { get; set; }
+        // TODO: Task 3: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
 
-            // TODO: Task 3: Add the two necessary constructors.
+        public Job(string Name, Employer EmployerName, Location EmployerLocation, PositionType JobType, CoreCompetency JobCoreCompetency) : this()
+        {
+            this.Name = Name;
+            this.EmployerName = EmployerName;
+            this.EmployerLocation = EmployerLocation;
+            this.JobType = JobType;
+            this.JobCoreCompetency = JobCoreCompetency;
+        }
 
-            // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
 
-            // TODO: Task 5: Generate custom ToString() method.
-                //Until you create this method, you will not be able to print a job to the console.
+        // TODO: Task 3: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        // TODO: Task 5: Generate custom ToString() method.
+        //Until you create this method, you will not be able to print a job to the console.
 
 
-        */
+
     }
 }
 
